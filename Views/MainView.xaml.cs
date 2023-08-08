e136bbb8c5b1d77af04a5cd7b82cc3c04049fc2f
Henrique Alves de Fernando <h236538@dac.unicamp.br>
2023-08-08 16:25:31 -0300

    [FEATURE] Mouse and keyboard events

--- Views/MainView.xaml.cs
+++ Views/MainView.xaml.cs
@@ -20,10 +20,164 @@ namespace Trabalho_final
     /// </summary>
     public partial class Calculadora : Window
     {
+       private string equation;
        
         public Calculadora()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            equation += button.Content;
+            Display.Content = equation;
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Square(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Negation(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void AddComma(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Erase(object sender, RoutedEventArgs e)
+        {
+            equation = equation.Substring(0, equation.Length - 1);
+            Display.Content = equation;
+
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            equation = "";
+            Display.Content = equation;
+        }
+
+        private void Constant(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Content.Equals("e"))
+            {
+                equation += "2.71828";
+            }
+            else
+            {
+                equation += "3.14159";
+            }
+            Display.Content = equation;
+        }
+
+        private void Window_KeyDownPreview(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                // Lógica para números
+                case Key.NumPad0: // 0
+                    equation += "0";
+                    break;
+                case Key.NumPad1:  // 1
+                    equation += "1";
+                    break;
+                case Key.NumPad2: // 2
+                    equation += "2";
+                    break;
+                case Key.NumPad3: // 3
+                    equation += "3";
+                    break;
+                case Key.NumPad4: // 4
+                    equation += "4";
+                    break;
+                case Key.NumPad5: // 5
+                    equation += "5";
+                    break;
+                case Key.NumPad6: // 6  
+                    equation += "6";
+                    break;
+                case Key.NumPad7: // 7
+                    equation += "7";
+                    break;
+                case Key.NumPad8: // 8
+                    equation += "8";
+                    break;
+                case Key.NumPad9: // 9
+                    equation += "9";
+                    break;
+
+                // Lógica para operadores
+                case Key.Add: // +
+                    equation += "+";
+                    break;
+                case Key.Subtract: // -
+                    equation += "-";
+                    break;
+                case Key.Multiply: // *
+                    equation += "*";
+                    break;
+                case Key.Divide: // /
+                    equation += "/";
+                    break;
+                case Key.Enter: // =
+                    equation += "=";
+                    break;
+
+                // Lógica para outros
+                case Key.P: // p
+                    equation += "3.14159";
+                    break;
+                case Key.E: // e
+                    equation += "2.71828";
+                    break;
+                case Key.Escape: // Esc
+                    equation = "";
+                    break;
+                case Key.Delete: // Delete
+                case Key.Back: // Backspace
+                    equation = equation.Substring(0, equation.Length - 1);
+                    break;
+                case Key.S: // s
+                    equation += "sin";
+                    break;
+                case Key.C: // c
+                    equation += "cos";
+                    break;
+                case Key.L: // log
+                    equation += "log";
+                    break;
+                case Key.Oem8: // ^
+                    equation += "power";
+                    break;
+                case Key.OemComma: // ,
+                    equation += ",";
+                    break;
+                case Key.OemBackslash: // \
+                    equation += "-1";
+                    break;
+                case Key.D9: // "("
+                    equation += "(";
+                    break;
+                case Key.D0: // "("
+                    equation += ")";
+                    break;
+                case Key.Up: // ~
+                    equation += "^";
+                    break;
+                
+                default:
+                    break;
+            }
+            Display.Content = equation;
         }
 
     }
