2c74e2e84e3b1fbbe56792edfe1283a5362b5d1c
Marcos Morais <moraismarcos727@gmail.com>
2023-08-24 21:40:29 -0300

    [FIX] Tratamento de excecao inicio negativo

--- Views/MainView.xaml.cs
+++ Views/MainView.xaml.cs
@@ -42,17 +42,25 @@ namespace Trabalho_final
 
         private void Calculate(object sender, RoutedEventArgs e)
         {
-            var calculator = new Trabalho_final.Controller.CalculatorController();
-            equation = equation + " = " + calculator.Calculate(equation).ToString();
-            Display.Content = equation;
-            //PERSISTE DADOS SQL SERVER
-            int position = equation.IndexOf("=") + 1;
-            string resultado_equation = equation.Substring(position).Trim();
-            string equation_body = equation.Substring(0, position).Trim();
-           // conexao.getDBConnection("insert into historico_calc(dt_atualizacao, equacao, resultado) VALUES(SYSDATETIME(), '" + equation_body + "', '" + resultado_equation + "');", "inserir");
-          //  conexao.equacao_history = "";
-          //  conexao.getDBConnection("SELECT TOP 5 format(dt_atualizacao,'dd/MM/yyyy HH:mm') AS data_atu, equacao, resultado FROM historico_calc ORDER BY dt_atualizacao desc;", "selecionar");
-            History.Text = conexao.equacao_history;
+            try
+            {
+                var calculator = new Trabalho_final.Controller.CalculatorController();
+                equation = equation + " = " + calculator.Calculate(equation).ToString();
+                Display.Content = equation;
+                //PERSISTE DADOS SQL SERVER
+                int position = equation.IndexOf("=") + 1;
+                string resultado_equation = equation.Substring(position).Trim();
+                string equation_body = equation.Substring(0, position).Trim();
+                
+            // conexao.getDBConnection("insert into historico_calc(dt_atualizacao, equacao, resultado) VALUES(SYSDATETIME(), '" + equation_body + "', '" + resultado_equation + "');", "inserir");
+            //  conexao.equacao_history = "";
+            //  conexao.getDBConnection("SELECT TOP 5 format(dt_atualizacao,'dd/MM/yyyy HH:mm') AS data_atu, equacao, resultado FROM historico_calc ORDER BY dt_atualizacao desc;", "selecionar");
+                History.Text = conexao.equacao_history;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in Calculate: " + ex.ToString());
+            }
 
         }
 
@@ -137,106 +145,112 @@ namespace Trabalho_final
 
         private void Window_KeyDownPreview(object sender, KeyEventArgs e)
         {
-            switch (e.Key)
+            try
             {
-                // Lógica para números
-                case Key.NumPad0: // 0
-                    equation += "0";
-                    break;
-                case Key.NumPad1:  // 1
-                    equation += "1";
-                    break;
-                case Key.NumPad2: // 2
-                    equation += "2";
-                    break;
-                case Key.NumPad3: // 3
-                    equation += "3";
-                    break;
-                case Key.NumPad4: // 4
-                    equation += "4";
-                    break;
-                case Key.NumPad5: // 5
-                    equation += "5";
-                    break;
-                case Key.NumPad6: // 6  
-                    equation += "6";
-                    break;
-                case Key.NumPad7: // 7
-                    equation += "7";
-                    break;
-                case Key.NumPad8: // 8
-                    equation += "8";
-                    break;
-                case Key.NumPad9: // 9
-                    equation += "9";
-                    break;
+                switch (e.Key)
+                {
+                    // Lógica para números
+                    case Key.NumPad0: // 0
+                        equation += "0";
+                        break;
+                    case Key.NumPad1:  // 1
+                        equation += "1";
+                        break;
+                    case Key.NumPad2: // 2
+                        equation += "2";
+                        break;
+                    case Key.NumPad3: // 3
+                        equation += "3";
+                        break;
+                    case Key.NumPad4: // 4
+                        equation += "4";
+                        break;
+                    case Key.NumPad5: // 5
+                        equation += "5";
+                        break;
+                    case Key.NumPad6: // 6  
+                        equation += "6";
+                        break;
+                    case Key.NumPad7: // 7
+                        equation += "7";
+                        break;
+                    case Key.NumPad8: // 8
+                        equation += "8";
+                        break;
+                    case Key.NumPad9: // 9
+                        equation += "9";
+                        break;
 
-                // Lógica para operadores
-                case Key.Add: // +
-                    equation += "+";
-                    break;
-                case Key.Subtract: // -
-                    equation += "-";
-                    break;
-                case Key.Multiply: // *
-                    equation += "*";
-                    break;
-                case Key.Divide: // /
-                    equation += "/";
-                    break;
-                case Key.Enter: // =
-                    equation += "=";
-                    //EXEMPLO USO CONEXAO SQL SERVER -- WILL
-                    //conexao.getDBConnection("insert into historico_calc(dt_atualizacao, equacao, resultado) VALUES(SYSDATETIME(), '" + equation + "', '---');", "inserir");
-                    break;
+                    // Lógica para operadores
+                    case Key.Add: // +
+                        equation += "+";
+                        break;
+                    case Key.Subtract: // -
+                        equation += "-";
+                        break;
+                    case Key.Multiply: // *
+                        equation += "*";
+                        break;
+                    case Key.Divide: // /
+                        equation += "/";
+                        break;
+                    case Key.Enter: // =
+                        equation += "=";
+                        //EXEMPLO USO CONEXAO SQL SERVER -- WILL
+                        //conexao.getDBConnection("insert into historico_calc(dt_atualizacao, equacao, resultado) VALUES(SYSDATETIME(), '" + equation + "', '---');", "inserir");
+                        break;
 
-                // Lógica para outros
-                case Key.P: // p
-                    equation += "3.14159";
-                    break;
-                case Key.E: // e
-                    equation += "2.71828";
-                    break;
-                case Key.Escape: // Esc
-                    equation = "";
-                    break;
-                case Key.Delete: // Delete
-                case Key.Back: // Backspace
-                    equation = equation.Substring(0, equation.Length - 1);
-                    break;
-                case Key.S: // s
-                    equation += "sin";
-                    break;
-                case Key.C: // c
-                    equation += "cos";
-                    break;
-                case Key.L: // log
-                    equation += "log";
-                    break;
-                case Key.Oem8: // ^
-                    equation += "power";
-                    break;
-                case Key.OemComma: // ,
-                    equation += ",";
-                    break;
-                case Key.OemBackslash: // \
-                    equation += "-1";
-                    break;
-                case Key.D9: // "("
-                    equation += "(";
-                    break;
-                case Key.D0: // "("
-                    equation += ")";
-                    break;
-                case Key.Up: // ^
-                    equation += "^";
-                    break;
-                
-                default:
-                    break;
+                    // Lógica para outros
+                    case Key.P: // p
+                        equation += "3.14159";
+                        break;
+                    case Key.E: // e
+                        equation += "2.71828";
+                        break;
+                    case Key.Escape: // Esc
+                        equation = "";
+                        break;
+                    case Key.Delete: // Delete
+                    case Key.Back: // Backspace
+                        equation = equation.Substring(0, equation.Length - 1);
+                        break;
+                    case Key.S: // s
+                        equation += "sin";
+                        break;
+                    case Key.C: // c
+                        equation += "cos";
+                        break;
+                    case Key.L: // log
+                        equation += "log";
+                        break;
+                    case Key.Oem8: // ^
+                        equation += "power";
+                        break;
+                    case Key.OemComma: // ,
+                        equation += ",";
+                        break;
+                    case Key.OemBackslash: // \
+                        equation += "-1";
+                        break;
+                    case Key.D9: // "("
+                        equation += "(";
+                        break;
+                    case Key.D0: // "("
+                        equation += ")";
+                        break;
+                    case Key.Up: // ^
+                        equation += "^";
+                        break;
+                    
+                    default:
+                        break;
+                }
+                Display.Content = equation;
             }
-            Display.Content = equation;
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in Window_KeyDownPreview: " + ex.ToString());
+            }    
         }
-
     }
 }
